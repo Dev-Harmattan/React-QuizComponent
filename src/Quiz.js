017161d40565050237e4cad519801d303e901923
= <ganiyusodiq059@gmail.com>
2020-09-27 19:03:22 -1200

    Completed module

--- src/Quiz.js
+++ src/Quiz.js
@@ -13,10 +13,10 @@ class Quiz extends Component {
     }
   }
   render() {
-    const isQuizEnd = (this.quiz_position - 1) === quizData.quiz_questions.length ? true : false;
+    const isQuizEnd = (this.quiz_position - 1) === quizData.quiz_questions.length;
     return (
       <div>
-        {isQuizEnd} ? <QuizEnd /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
+        {isQuizEnd ? <QuizEnd /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />}
       </div>
     )
   }
