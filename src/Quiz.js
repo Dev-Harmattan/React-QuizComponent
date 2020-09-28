39a625e70976515842c52fa82d1820e0a75c9efe
= <ganiyusodiq059@gmail.com>
2020-09-27 18:58:49 -1200

    Completed module

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,26 +1,24 @@
-import React, { Component } from "react";
-import QuizQuestion from "./QuizQuestion";
-import QuizEnd from "./QuizEnd";
-let quizData = require("./quiz_data.json");
+import React, { Component } from 'react';
+import QuizQuestion from './QuizQuestion';
+import QuizEnd from './QuizEnd'
+let quizData = require('./quiz_data.json');
+
 
 class Quiz extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      quiz_position: 1,
-    };
+      quiz_position: 1
+    }
   }
   render() {
-    const isQuizEnd =
-      this.quiz_position - 1 === quizData.quiz_questions.length ? true : false;
-    return isQuizEnd ? (
-      <QuizEnd />
-    ) : (
-      <QuizQuestion
-        quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}
-      />
-    );
+    const isQuizEnd = (this.quiz_position - 1) === quizData.quiz_questions.length ? true : false;
+    return (
+      <div>
+        {isQuizEnd} ? <QuizEnd /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
+      </div>
+    )
   }
 }
 
